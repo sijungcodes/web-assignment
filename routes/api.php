d6f42c4a41dc36562e3f536ff44605056b54d5da
sijungcodes <sijung.nagano@outlook.com>
2021-04-22 13:45:25 +0900

    create api route to book controller's index method

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,4 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('books', 'BookController@index')->name('books.index');
