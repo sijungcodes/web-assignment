668f2fa6a915238b2c1762011482b7595a5ca794
sijungcodes <sijung.nagano@outlook.com>
2021-04-23 00:49:59 +0900

    Create facotry for author table

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use Faker\Generator as Faker;
+
+//Use Faker's catchPrase method to simulate book titles in testing or seeding.
+$factory->define(Author::class, function (Faker $faker) {
+    return [
+        'title' => $this->faker->name,
+    ];
+});
