02f65b455d301cf484f94ea6563cddae21cd3e56
sijungcodes <sijung.nagano@outlook.com>
2021-04-22 23:40:44 +0900

    Create route for searching books table

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,6 @@ use Illuminate\Http\Request;
 */
 
 Route::get('books', 'BookController@index')->name('books.index');
+Route::get('search-books/{query}', 'BookController@search')->name('books.search');
 Route::post('books', 'BookController@store')->name('books.store');
 Route::delete('books/{id}', 'BookController@delete')->name('books.delete');
