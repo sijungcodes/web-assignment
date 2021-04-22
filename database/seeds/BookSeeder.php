fb06f92b51957449b74ba20175a945b0e88543e2
sijungcodes <sijung.nagano@outlook.com>
2021-04-22 13:14:09 +0900

    Create seeder for book model

---
+++ database/seeds/BookSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Seed book table with book titles for testing.
+        $books = factory(App\Book::class, 10)->create();
+    }
+}
