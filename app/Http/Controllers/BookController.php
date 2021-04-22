c9fc55f24292a2236f6b6c0746249a116a48a5f5
sijungcodes <sijung.nagano@outlook.com>
2021-04-22 15:36:37 +0900

    Fix bugs in book controller
     - import book model
     - use Book model instead of DB to interface with books table

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {   
-        $books = DB::table('books', 'desc')->take(50)->get();
+        $books = Book::orderBy('id', 'desc')->take(50)->get();
         return $books->toJson();
     }
     
