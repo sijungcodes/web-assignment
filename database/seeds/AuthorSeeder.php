0ece10eaa6fca8676a208b855d29626c32a83db8
sijungcodes <sijung.nagano@outlook.com>
2021-04-23 00:56:35 +0900

    Create seeder for authors table

---
+++ database/seeds/AuthorSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Seed author table with author name for testing.
+        $authors = factory(App\Author::class, 10)->create();  
+    }
+}
