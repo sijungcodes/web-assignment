14645f80a0155fdefdd7093a67f1b9ff1d3dbc39
sijungcodes <sijung.nagano@outlook.com>
2021-04-22 12:44:39 +0900

    Create book model and migration for said model

---
+++ app/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title', 
+    ]; 
+    
+    public function authors()
+    {
+        return $this->belongsToMany(Author::class);
+    }
+}
