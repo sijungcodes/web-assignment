13fc7f71458eaa48078a95a479e7611bf4087026
sijungcodes <sijung.nagano@outlook.com>
2021-04-23 14:44:41 +0900

    Create migration for book author pivot table

---
+++ database/migrations/2021_04_23_054051_create_book_author_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_author', function (Blueprint $table) {
+            $table->unique(['author_id', 'book_id']);
+            $table->integer('author_id')->unsigned();
+            $table->integer('book_id')->unsigned();  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_author');
+    }
+}
