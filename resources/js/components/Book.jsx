f546ea90bdca1ec0f5d62c0b1b0f6981cfd29010
sijungcodes <sijung.nagano@outlook.com>
2021-04-24 20:53:31 +0900

    Create book react component

---
+++ resources/js/components/Book.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Book = (props) => {
+    return (
+            <div className=" ">
+                <div className=" ">{props.title}</div>
+            </div>
+    );  
+};
+
+export default Book;
