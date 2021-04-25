e12964911533e544bb87feb7451dbc77826006b8
sijungcodes <sijung.nagano@outlook.com>
2021-04-25 16:16:41 +0900

    Add author field and styles
     - add author name field
     - validate date before send to server
     - add css style

--- resources/js/components/AddBook.jsx
+++ resources/js/components/AddBook.jsx
@@ -27,27 +27,24 @@ class AddBook extends Reflux.Component {
       //confirm("Add " + this.state.title + " by " + this.state.author);
       if(this.state.title !== ''){
         if(this.state.author){
-          BookActions.addBook(this.state.title, this.state.aurthor);
+          BookActions.addBook(this.state.title, this.state.author);
+          this.setState({title:'', author: ''});
         }else{
-          alert("Book author field cannot be blank");
+          alert("Author field cannot be blank");
         }      
       }else{
-        alert("Book title field cannot be blank");
+        alert("Title field cannot be blank");
       }
     }
   
     render() {
       return (
-        <div className="center mw5 mw6-ns hidden ba mv4">   
+        <div className=" mw5 mw6-ns hidden ba mv4">   
           <h1 className="f4 bg-near-black white mv0 pv2 ph3">Add Book</h1> 
           <div className="pa3 bt">   
-              <label className="db fw6 lh-copy f6">
-                Title:</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" value={this.state.title} onChange={this.handleTitleChange} />
-              
-              <label className="db fw6 lh-copy f6">
-                Author:</label>   
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" value={this.state.author} onChange={this.handleAuthorChange} />
+                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" value={this.state.title} onChange={this.handleTitleChange} placeholder="title"/>
+            
+                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" value={this.state.author} onChange={this.handleAuthorChange} placeholder="author"/>
                      
               <button className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" onClick={this.handleAddBook} >Add book</button>
           </div>
