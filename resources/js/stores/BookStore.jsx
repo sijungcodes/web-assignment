cd86977b11900df651d6a2cc9ec01a7f6f4da70d
sijungcodes <sijung.nagano@outlook.com>
2021-04-27 11:48:12 +0900

    Refactor bookstore
     - combine search book and get initial book function into one and use optional parameter
     - set base url in constructor

--- resources/js/stores/BookStore.jsx
+++ resources/js/stores/BookStore.jsx
@@ -5,52 +5,48 @@ class BookStore extends Reflux.Store {
     constructor() {
         super();
         this.listenables = BookActions;
+        this.url = '/api/books';
         this.state = {
-            books: [{"id":45,"title":"my book title","created_at":"2021-04-24 00:37:52","updated_at":"2021-04-24 00:37:52"}],
+            sort: 'asc',
+            searchBy: 'title',
+            query: '',
+            books: [],
             note: 'hello'
         }
     }
 
-    onGetInitialBookData() {
-
-        var url = "http://localhost/api/books";
-
-        fetch(url, { method: 'GET' })
-            .then(res => res.text())
-            .then(res => this.setState({
-                books: JSON.parse(res).map(function(item, index) {
-                    return { id: item.id, title: item.title}
-                })
-            }))
-            .catch(err => err);
+    setBooksState(res){
+        this.setState({
+            books: JSON.parse(res).map(function(item, index) {
+                return { id: item.id, title: item.title, authorName: item.authors[0].name, authorId:item.authors[0].id }
+            })
+        })
     }
 
-    onAddBook(bookTitle){
-        var url = "http://localhost/api/books";
+    onToggleSort(sort){
+        this.setState({sort: sort});
+        this.onSearchBooks('');
+    } 
 
+    onAddBook(bookTitle, bookAuthor){
+       
         var requestData = {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json, text-plain, */*",
                 "X-Requested-With": "XMLHttpRequest",
                 },
-            method: 'post',
+            method: "post",
             credentials: "same-origin",
-            body: JSON.stringify({title: bookTitle})
+            body: JSON.stringify({title: bookTitle, author: bookAuthor})
         }
-        fetch(url, requestData)
+        fetch(this.url, requestData)
             .then(res => res.text())
-            .then(res => this.setState({
-                books: JSON.parse(res).map(function(item, index) {
-                    return { id: item.id, title: item.title}
-                })
-            }))
+            .then(res => this.setBooksState(res))
             .catch(err => err);        
     }
 
     onRemoveBook(bookId){
-        var url = "http://localhost/api/books";
-        alert(bookId);
         var requestData = {
             headers: {
                 "Content-Type": "application/json",
@@ -58,31 +54,23 @@ class BookStore extends Reflux.Store {
                 "X-Requested-With": "XMLHttpRequest",
                 "_method": "DELETE"
                 },
-            method: 'post',
+            method: "post",
             credentials: "same-origin",
             body: JSON.stringify({_method:  "DELETE",  id: bookId})
         }
-        fetch(url, requestData)
+        fetch(this.url, requestData)
             .then(res => res.text())
-            .then(res => this.setState({
-                books: JSON.parse(res).map(function(item, index) {
-                    return { id: item.id, title: item.title}
-                })
-            }))
+            .then(res => this.setBooksState(res))
             .catch(err => err);          
     }
 
     onSearchBooks(query){
         var query = encodeURIComponent(query);
-        var url = "http://localhost/api/search-books/" + query;
-
+        this.setState({query: query});
+        var url = this.url + '/' + this.state.sort +'/'+ this.state.searchBy  + '/' + query;
         fetch(url, { method: 'GET' })
             .then(res => res.text())
-            .then(res => this.setState({
-                books: JSON.parse(res).map(function(item, index) {
-                    return { id: item.id, title: item.title}
-                })
-            }))
+            .then(res => this.setBooksState(res))
             .catch(err => err);
     }
 }
