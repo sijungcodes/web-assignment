c8555ccafdb132050d22972050e7e1306e1872df
sijungcodes <sijung.nagano@outlook.com>
2021-04-29 14:45:11 +0900

    Create repository for book related model operations

---
+++ app/Repositories/BookRepository.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Repositories;
+
+use App\book;
+use App\Author;
+
+class BookRepository
+{
+    /**
+     * Get books that match query.
+     *
+     * @param  String $query
+     * @param  String $sort
+     * @return Collection
+     */
+    public function queryTitle($query, $sort = 'asc')
+    {
+        $books = Book::whereRaw(
+                "MATCH(title) AGAINST(?)", 
+                array($query)
+        )->with('authors')->orderBy('title', $sort)->get();
+        
+        return $books;
+    }
+
+    /**
+     * Get all books.
+     *
+     * @param  String $sort
+     * @return Collection
+     */
+    public function getSortedBy($sort = 'asc')
+    {
+        return Book::with('authors')->orderBy('title', $sort)->get();
+    }
+
+    /**
+     * Create a new book record and link book to author.
+     *
+     * @param  String $bookTitle
+     * @param  String $authorName
+     * @return Book
+     */
+    public function storeAndLinkToAuthor($bookTitle, $authorName)
+    {   
+        $author = new Author;
+        $author->name = $authorName; 
+        $author->save();   
+
+        $book = new Book;
+        $book->title = $bookTitle; 
+        $book->save();   
+        
+        $book->authors()->syncWithoutDetaching(
+            $author->id
+        );        
+
+        return $book;
+    }   
+
+    /**
+     * Link book to author.
+     *
+     * @param  Book $book
+     * @param  Int $authorId
+     * @return Array
+     */
+    public function linkToAuthor($book, $authorId)
+    {    
+        return $book->authors()->syncWithoutDetaching(
+            $authorId
+        );
+    }             
+
+    /**
+     * Link book to author.
+     *
+     * @param  Int $bookId
+     * @return Boolean
+     */
+    public function delete($bookId)
+    {    
+        $book = Book::find($bookId);
+        $book->authors()->detach();
+        return $book->delete();
+    }     
+
+}
