fe2f1859e2d36dd6f75698b28b86f8f6dadf66f8
sijungcodes <sijung.nagano@outlook.com>
2021-05-01 15:56:53 +0900

    Change css in header

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,7 +1,7 @@
     import React from 'react'
 
     const Header = () => ( 
-        <nav className="white bg-black sans-serif db"><div className="pa3 mw9 f1 center">Books and Authors</div> </nav>
+        <nav className="white bg-black"><div className="pa3 mw9 f1 center">Books and Authors</div> </nav>
     )
 
     export default Header
