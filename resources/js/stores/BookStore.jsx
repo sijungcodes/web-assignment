ffc0589caadad9db46e850b8f58bfa6e00937ebe
sijungcodes <sijung.nagano@outlook.com>
2021-05-01 15:45:35 +0900

    Refactor react store

--- resources/js/stores/BookStore.jsx
+++ resources/js/stores/BookStore.jsx
@@ -3,86 +3,46 @@ import BookActions from '../actions/BookActions';
 import xmljs from 'xml-js';
 import objectToCsv from 'object-to-csv';
 
-
 class BookStore extends Reflux.Store {
     constructor() {
         super();
         this.listenables = BookActions;
         this.urlBooks = '/api/books';
+        this.urlAuthors = '/api/authors';
         this.state = {
+            querySelect: 'title',
             sort: 'asc',
-            searchBy: 'title',
-            query: '',
+            search: '',
             books: [],
         }
     }
 
-    getBooks(){
-        var url = this.urlBooks + '/' + this.state.sort +'/'+ this.state.searchBy  + '/' + this.state.query;
-        fetch(url, { method: 'GET' })
-            .then(res => res.text())
-            .then(res => this.setBooksState(res))
-            .catch(err => err);        
-    }
-
     setBooksState(res){
         this.setState({
-            books: JSON.parse(res).map(function(item) {
-                return { id: item.id, title: item.title, authorName: item.authors[0].name, authorId: item.authors[0].id }
+            books: JSON.parse(res).map(function(book) {
+                return { id: book.id, 
+                        title: book.title,
+                        authorName: book.authors[0].name, 
+                        authorId: book.authors[0].id 
+                }
             })
         })
     }
     
-    setAuthorName(res){
-
-        //Fine in books array book object that has a matching id  
+    setAuthorName(res)
+    {
         function findMatchingAuthorId(book) {
-            return book.authorId == res.author_id;
+            return book.authorId == res.id;
         }
+        //Find author by id within books array
         var indexOfMatch = this.state.books.findIndex(findMatchingAuthorId);
 
-        //Create updated books array and set state
+        //Create up to date books array and set state
         this.state.books[indexOfMatch].authorName = res.name;
         this.setState({books: this.state.books});
-
     }
-
-    constructArrayOfObjects(keys){
-
-        return this.state.books.map(function(book){
-            var objectToBeReturned = {};
-            for(var i = 0; i < keys.length; i++){
-                if (keys[i] in book){
-                    objectToBeReturned[keys[i]] = book[keys[i]];
-                }
-            }
-            return objectToBeReturned;
-        });
-    }
-
-    async onConvertObjectToCSV(keyArray){
-        let data = this.constructArrayOfObjects(keyArray);
-        let keys = Object.keys(data[0]).map((key) => ({ key: key, as: key }));
-        var otc = new objectToCsv({ keys: keys,data: data });
-        var csv = await otc.getCSV();
-        window.location = "data:application/octet-stream," + encodeURIComponent(csv);
-        return csv;
-    }
-
-    async onConvertObjectToXML(keyArray){
-        let data = this.constructArrayOfObjects(keyArray);
-        var options = {compact: true, ignoreComment: true, spaces: 4};
-        var xml = await xmljs.js2xml(data, options);
-        window.location = "data:application/octet-stream," + encodeURIComponent(xml);
-        return  xml;
-    }
-
-    onToggleSort(sort){
-        this.setState({sort: sort});
-    } 
 
     onAddBook(bookTitle, bookAuthor){
-       
         var requestData = {
             headers: {
                 "Content-Type": "application/json",
@@ -117,15 +77,31 @@ class BookStore extends Reflux.Store {
             .catch(err => err);          
     }
 
-    onSearchBooks(query){
-        var query = encodeURIComponent(query);
-        this.setState({query: query});
-        this.getBooks();
+    onSetQuerySelect(querySelect){
+        this.setState({querySelect: querySelect});
+    }   
+
+    onGetBySort(sort){
+        this.setState({sort: sort});
+        this.onRequestBooks();
+    }  
+
+    onGetBySearch(search){
+        this.setState({search: search});
+        this.onRequestBooks(search);
+    }      
+
+    onRequestBooks(){
+        var url = this.urlBooks + '/by-'+this.state.querySelect+'/' + this.state.sort + '/' + encodeURIComponent(this.state.search)
+        fetch(url, { method: 'GET' })
+            .then(res => res.text())
+            .then(res => this.setBooksState(res))
+            .catch(err => err);          
     }
 
     onUpdateAuthor(authorId, authorName){
 
-        var url = "/api/update-authors";
+        var url = this.urlAuthors + "/update";
         
         var requestData = {
             headers: {
@@ -141,8 +117,39 @@ class BookStore extends Reflux.Store {
             .then(res => res.text())
             .then(res => this.setAuthorName(JSON.parse(res)))
             .catch(err => err);    
-                
-    }        
+    }   
+
+    constructArrayOfObjects(keys){
+        
+        return this.state.books.map(function(book){
+            var objectToBeReturned = {};
+
+            for(var i = 0; i < keys.length; i++){
+                if (keys[i] in book){
+                    objectToBeReturned[keys[i]] = book[keys[i]];
+                }
+            }
+            return objectToBeReturned;
+        });
+    }    
+
+    async onConvertObjectToCSV(keyArray){
+        let data = this.constructArrayOfObjects(keyArray);
+        let keys = Object.keys(data[0]).map((key) => ({ key: key, as: key }));
+        var otc = new objectToCsv({ keys: keys,data: data });
+        var csv = await otc.getCSV();
+        window.location = "data:application/csv," + encodeURIComponent(csv);
+        return csv;
+    }
+
+    async onConvertObjectToXML(keyArray){
+        let data = this.constructArrayOfObjects(keyArray);
+        var options = {compact: true, ignoreComment: true, spaces: 4};
+        var xml = await xmljs.js2xml(data, options);
+        window.location = "data:application/octet-stream," + encodeURIComponent(xml);
+        return  xml;
+    }    
+    
 }
 
 export default BookStore;
